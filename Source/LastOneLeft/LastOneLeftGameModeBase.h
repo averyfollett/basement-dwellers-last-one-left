b403fc058787adbb09f44b7d95e86dfc8f582b05
Avery Follett <avery.follett@mymail.champlain.edu>
2020-05-15 15:29:11 -0400

    initial unreal project commit

---
+++ Source/LastOneLeft/LastOneLeftGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LastOneLeftGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LASTONELEFT_API ALastOneLeftGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
